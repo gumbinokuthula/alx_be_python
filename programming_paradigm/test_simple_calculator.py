116a54c8780bcf6f7d7804066d48b3414afbe331
Nokuthula Gumbi <nokuthulagumbi737@gmail.com>
2025-08-03 16:38:52 +0200

    Rename test methods to match checker expectations

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -5,36 +5,29 @@ from simple_calculator import SimpleCalculator
 
 
 class TestSimpleCalculator(unittest.TestCase):
-    """ Unit tests for the SimpleCalculator class. """
 
     def setUp(self):
-        """ Create a SimpleCalculator instance before each test. """
         self.calc = SimpleCalculator()
 
     def test_addition(self):
-        """ Test the add() method. """
         self.assertEqual(self.calc.add(1, 2), 3)
         self.assertEqual(self.calc.add(-1, -1), -2)
         self.assertEqual(self.calc.add(-5, 5), 0)
         self.assertEqual(self.calc.add(0, 0), 0)
 
     def test_subtraction(self):
-        """ Test the subtract() method. """
         self.assertEqual(self.calc.subtract(5, 3), 2)
         self.assertEqual(self.calc.subtract(3, 5), -2)
         self.assertEqual(self.calc.subtract(-2, -2), 0)
 
-    def test_multiply(self):
-        """ Test the multiply() method. """
+    def test_multiplication(self):
         self.assertEqual(self.calc.multiply(4, 5), 20)
         self.assertEqual(self.calc.multiply(-1, 5), -5)
         self.assertEqual(self.calc.multiply(0, 99), 0)
 
-    def test_divide(self):
-        """ Test the divide() method. """
+    def test_division(self):
         self.assertEqual(self.calc.divide(10, 2), 5)
         self.assertEqual(self.calc.divide(-9, 3), -3)
-        # Edge case: dividing by zero
         self.assertIsNone(self.calc.divide(10, 0))
 
 
